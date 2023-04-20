941e381dbdf093b45e979e2cedcdc34210361024
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-20 11:04:09 -0400

    Create training_curves.py

---
+++ training_curves.py
@@ -0,0 +1,14 @@
+import json
+import pickle
+import numpy as np 
+
+# different paths of checkpoints folders for analysis 
+v1 = 'equivariant_all_bn_v1_v2'
+v2 = 'equivariant_all_bn_v2_v2'
+v3 = 'equivariant_all_bn_v3_v2'
+faces_texture = 'factorize_avgpool_equivariant_all_bn_v5'
+
+
+path = '/home/ec3731/checkpoints/barlowtwins/equivariant_all_bn_v1_v2/stats.txt'
+
+
