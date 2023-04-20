1a99be9a202c64a1f40ecbec16a93172c82baa2f
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-20 11:48:24 -0400

    Update training_curves.py

--- training_curves.py
+++ training_curves.py
@@ -33,7 +33,7 @@ print('len: ', len(list_lines))
 list_losses = []
 for line in list_lines:
   print(line.type)
-  loss = line["loss"]
+  loss = json.loads(line)["loss"]
   list_losses.append(loss)
   
 
