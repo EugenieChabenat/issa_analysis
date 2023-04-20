60abb3b6dc8360472329d01a774d692a6b7fd4df
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-20 12:59:45 -0400

    Update training_curves.py

--- training_curves.py
+++ training_curves.py
@@ -10,7 +10,7 @@ v3 = 'equivariant_all_bn_v3_v2'
 faces_texture = 'factorize_avgpool_equivariant_all_bn_v5'
 
 
-path = '/home/ec3731/checkpoints/barlowtwins/equivariant_all_bn_v1_v2/stats.txt'
+path = '/home/ec3731/checkpoints/barlowtwins/factorize_avgpool_equivariant_all_bn_v5/stats.txt'
 
 list_lines = []
 
