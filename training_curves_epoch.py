267f4d524c7a3ca62c25a67979a2e9cdf7bb8eec
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-27 16:41:38 -0400

    Update training_curves_epoch.py

--- training_curves_epoch.py
+++ training_curves_epoch.py
@@ -59,7 +59,8 @@ for line in list_lines:
       inds.append(ind)
     ind = 0 
     current_e =line["epoch"]
-
+    
+plt.figsize=(20, 20)
 plt.plot(losses)  
 plt.xticks(ticks= inds, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29])
 plt.tight_layout()
