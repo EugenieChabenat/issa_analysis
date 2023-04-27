65651703379abaf146dd2c08170bf20976408aaa
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-27 16:42:28 -0400

    Update training_curves_epoch.py

--- training_curves_epoch.py
+++ training_curves_epoch.py
@@ -60,10 +60,10 @@ for line in list_lines:
     ind = 0 
     current_e =line["epoch"]
     
-plt.figsize=(20, 20)
+plt.figsize=(30, 30)
 plt.plot(losses)  
 plt.xticks(ticks= inds, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29])
-plt.tight_layout()
+#plt.tight_layout()
 plt.title('training curve imagenet factorize avgpool equivariant')
 plt.show()
 plt.savefig('/mnt/smb/locker/issa-locker/users/Eugénie/nn-analysis/training_curves/losstest1.png')
