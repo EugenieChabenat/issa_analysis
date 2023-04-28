fe7c9f59468327b848d6f80e9ffbfb1ade2794e8
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-28 09:45:02 -0400

    Update training_curves_epoch.py

--- training_curves_epoch.py
+++ training_curves_epoch.py
@@ -63,7 +63,7 @@ for line in list_lines:
         inds.append(ind)
       ind = 0 
       current_e =line["epoch"]
-
+print(current_e) 
 labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
 #labels = np.arange(0, 29, step=1)
 new_labels = []
