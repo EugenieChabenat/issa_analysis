bb40740f01b293edf44c825082e96facbc6f5091
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-20 16:07:24 -0400

    Update npy_to_images.py

--- npy_to_images.py
+++ npy_to_images.py
@@ -7,7 +7,7 @@ path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HK2/HK2_imag
 img_array = np.load(path)
 print('size: ', img_array.shape)
 
-for i in range(25):
+for i in range(img_array.shape[0]):
   plt.imshow(img_array[i], cmap='gray')
   im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/hk2/image{}.png'.format(i), img_array[i]) 
   plt.show()
