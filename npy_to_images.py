9cf6d14f1c4c978e6de5b68737c21aedd2234c96
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-20 16:33:37 -0400

    Update npy_to_images.py

--- npy_to_images.py
+++ npy_to_images.py
@@ -3,20 +3,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.image as im
 
+save_images = False 
 
 # --------------------------------------------------------------------------------------------------
 # HK2 DATASET 
 # --------------------------------------------------------------------------------------------------
-"""path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HK2/HK2_images.npy'
+path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HK2/HK2_images.npy'
 img_array = np.load(path)
 print('size: ', img_array.shape)
 
-for i in range(img_array.shape[0]):
-  plt.imshow(img_array[i], cmap='gray')
-  im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/hk2/image{}.png'.format(i), img_array[i]) 
-  plt.show()"""
+if save_images: 
+  for i in range(img_array.shape[0]):
+    plt.imshow(img_array[i], cmap='gray')
+    im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/hk2/image{}.png'.format(i), img_array[i]) 
+    plt.show()
 
-  
+path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HK2/HK2_classes.npy'
+classes_array = np.load(path)
+print('size: ', classes_array.shape)
+
+path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HK2/HK2_class_to_idx.npy'
+classes_to_idx_array = np.load(path)
+print('size: ', classes_to_idx_array.shape)
+
 # --------------------------------------------------------------------------------------------------
 # HVM DATASET 
 # --------------------------------------------------------------------------------------------------
@@ -24,23 +33,25 @@ for i in range(img_array.shape[0]):
 img_array = np.load(path)
 print('size: ', img_array.shape)
 
-for i in range(img_array.shape[0]):
-  plt.imshow(img_array[i], cmap='gray')
-  im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/hvm/image{}.png'.format(i), img_array[i]) 
-  plt.show()"""
+if save_images: 
+  for i in range(img_array.shape[0]):
+    plt.imshow(img_array[i], cmap='gray')
+    im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/hvm/image{}.png'.format(i), img_array[i]) 
+    plt.show()"""
   
 # --------------------------------------------------------------------------------------------------
 # MKTURK DATASET 
 # --------------------------------------------------------------------------------------------------
 
-path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/mkturk/test/images.npy'
+"""path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/mkturk/test/images.npy'
 img_array = np.load(path)
 print('size: ', img_array.shape)
 
 #img_array = img_array.reshape((432, 256, 256, 3))
-for j in range(img_array.shape[0]): 
-  for i in range(img_array.shape[1]):
-    plt.imshow(img_array[j][i].reshape((256, 256, 3)))
-    im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/mkturk_test/folder_{}/image{}.png'.format(j,i), img_array[j][i].reshape((256, 256, 3))) 
-    plt.show()
+if save_images: 
+  for j in range(img_array.shape[0]): 
+    for i in range(img_array.shape[1]):
+      plt.imshow(img_array[j][i].reshape((256, 256, 3)))
+      im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/mkturk_test/folder_{}/image{}.png'.format(j,i), img_array[j][i].reshape((256, 256, 3))) 
+      plt.show()"""
 
