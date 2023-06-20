9d0d5fbe5beff5d7532626e456243d0c6972d640
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-20 16:13:58 -0400

    Update npy_to_images.py

--- npy_to_images.py
+++ npy_to_images.py
@@ -20,19 +20,25 @@ for i in range(img_array.shape[0]):
 # --------------------------------------------------------------------------------------------------
 # HVM DATASET 
 # --------------------------------------------------------------------------------------------------
-path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HVM/HVM_images.npy'
+"""path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HVM/HVM_images.npy'
 img_array = np.load(path)
 print('size: ', img_array.shape)
 
 for i in range(img_array.shape[0]):
   plt.imshow(img_array[i], cmap='gray')
   im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/hvm/image{}.png'.format(i), img_array[i]) 
-  plt.show()
+  plt.show()"""
   
 # --------------------------------------------------------------------------------------------------
 # MKTURK DATASET 
 # --------------------------------------------------------------------------------------------------
 
-"""path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HK2/HK2_images.npy'
+path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/mkturk/test/images.npy'
 img_array = np.load(path)
-print('size: ', img_array.shape)"""
+print('size: ', img_array.shape)
+
+for i in range(img_array.shape[0]):
+  plt.imshow(img_array[i], cmap='gray')
+  im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/mkturk_test/image{}.png'.format(i), img_array[i]) 
+  plt.show()
+
