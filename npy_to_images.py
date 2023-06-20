49221e27762a5b1ba6e0d61412707775601938b2
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-20 16:04:36 -0400

    Update npy_to_images.py

--- npy_to_images.py
+++ npy_to_images.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from matplotlib import pyplot as plt
+import matplotlib.image as im
 
 path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HK2/HK2_images.npy'
 img_array = np.load(path)
@@ -8,4 +9,5 @@ print('size: ', img_array.shape)
 
 for i in range(3):
   plt.imshow(img_array[i], cmap='gray')
+  im.imsave('/mnt/smb/locker/issa-locker/users/Eugénie/images/hk2/image{}.png'.format(i), img_array[i]) 
   plt.show()
