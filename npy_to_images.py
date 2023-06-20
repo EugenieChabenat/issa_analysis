147bea38c6bdee96b56a1c3aa9892050ead12aae
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-06-20 15:57:23 -0400

    Create npy_to_images.py

---
+++ npy_to_images.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+path = '/mnt/smb/locker/issa-locker/users/hc3190/datasets/imagesets/HK2/HK2_images.npy'
+img_array = np.load(path)
+print('size: ', img_array.shape)
+
+for i in range(3):
+  plt.imshow(data[i], cmap='gray')
+  plt.show()
