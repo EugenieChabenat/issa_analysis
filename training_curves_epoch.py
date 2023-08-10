ee3dbe0254d7e85a4c4c1367f0ec3b3328120295
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-08-10 11:23:47 -0400

    Update training_curves_epoch.py

--- training_curves_epoch.py
+++ training_curves_epoch.py
@@ -78,7 +78,7 @@ faces_texture = 'factorize_avgpool_equivariant_all_bn_v5'
 path = '/mnt/smb/locker/issa-locker/users/Eugénie/models/checkpoints/barlowtwins/multiplicative_model_v1/stats.txt'
 path = '/mnt/smb/locker/issa-locker/users/Eugénie/models/checkpoints/barlowtwins/multiplicative_model_v2_v1/stats.txt'
 path = '/mnt/smb/locker/issa-locker/users/Eugénie/models/checkpoints/barlowtwins/multiplicative_model_v4_v1/stats.txt'
-#path = '/mnt/smb/locker/issa-locker/users/Eugénie/models/checkpoints/barlowtwins/multiplicative_model_IT_v1/stats.txt'
+path = '/mnt/smb/locker/issa-locker/users/Eugénie/models/checkpoints/barlowtwins/multiplicative_model_IT_v1/stats.txt'
 
 # -- 
 
